5c5dde79e738510df3a4ac7098f0ad67d413eea3
ellyvandertogt <100711482+ellyvandertogt@users.noreply.github.com>
2023-01-04 09:09:38 +0100

    fourth submit

--- script.js
+++ script.js
@@ -1,3 +1,34 @@
+//https://www.youtube.com/watch?v=AzvpHNkjqsg&ab_channel=KennyYipCoding
+
 var board;
 var playerO = "O";
 var playerx = "X";
+var currPlayer = "playerO";
+var gameOver = "false";
+
+window.onload = function() {
+    setGame();
+}
+
+function setGame() {
+    board = [
+        [" ", " ", " "]
+        [" ", " ", " "]
+        [" ", " ", " "]
+    ]
+
+    for (let r = 0; r < 3; r++) {
+        for (let c = 0; c < 3; c++) {
+        let tile = document.createElement("div");
+        tile.id = r.toString() + "-" + c.toString();
+        tile.classList.add("tile");
+        if (r == 0 || r == 1) {
+            tile.classList.add("horizontal-line");
+        }
+        if (c == 0 || c == 1 {
+            tile.classList.add("vertical-line");
+        }
+        document.getElementById("board").append(tile);
+        }
+    }
+}
