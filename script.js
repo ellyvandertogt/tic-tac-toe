c95720964d67df39aa817826a3989291dc631460
ellyvandertogt <100711482+ellyvandertogt@users.noreply.github.com>
2023-01-04 17:37:39 +0100

    d

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 //https://www.youtube.com/watch?v=AzvpHNkjqsg&ab_channel=KennyYipCoding
 
 var board;
-var playerO = "O";
+var PlayerO = "O";
 var playerx = "X";
-var currPlayer = "playerO";
-var gameOver = "false";
+var currPlayer = playerO;
+var gameOver = false;
 
 window.onload = function() {
     setGame();
@@ -25,10 +25,40 @@ function setGame() {
         if (r == 0 || r == 1) {
             tile.classList.add("horizontal-line");
         }
-        if (c == 0 || c == 1 {
+        if (c == 0 || c == 1) {
             tile.classList.add("vertical-line");
         }
+        tile.addEventListener("click", setTile);
         document.getElementById("board").append(tile);
         }
+    }
+}
+
+function setTile() {
+    if (gameOver) {
+        return;
+    }
+
+    let coords = this.id.split("-")//"1-1" -> ["1", "1"]
+    let r = parseInt(coords[0]);
+    let c = parseInt(coords[1]);
+    if (board[r][c] != " ") {
+        return;
+    }
+    board[r][c] = currPlayer;
+    this.innerText = currPlayer;
+
+    if (currPlayer == playerO) {
+        currPlayer = playerx;
+    } else {
+        currPlayer= PlayerO;
+    }
+
+    checkWinnar();
+}
+
+function checkWinnar() {
+    for (let r = 0; r < 3; r++) {
+        if (board[r][0] == board[r][1] && board[r][1] == board[r][2] && board[r][0] )
     }
 }
